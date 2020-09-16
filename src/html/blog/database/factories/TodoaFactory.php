d2072e445a3c199b087bc95eb886caec2cc42bc8
George Mylonas <mylonasg88@gmail.com>
2020-09-16 14:53:10 +0800

    Added PostgresQL to work with Laravel :)

---
+++ src/html/blog/database/factories/TodoaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Todoa;
+use Faker\Generator as Faker;
+
+$factory->define(Todoa::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
